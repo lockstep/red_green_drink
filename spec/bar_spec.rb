5fcb711ea5c1cf27d010def8ec31fdb1dd4c0d2d
Jan Klimo <jan.klimo@gmail.com>
2015-08-06 21:06:19 +0700

    tests round 2

--- spec/bar_spec.rb
+++ spec/bar_spec.rb
@@ -2,8 +2,23 @@
 require_relative "../bar"
 
 describe Bar do
+  before { @bar = Bar.new }
   it 'is open for business!' do
-    bar = Bar.new
-    expect(bar).to be_open_for_business
+    expect(@bar).to be_open_for_business
+  end
+  it 'has 80 seats' do
+    expect(@bar.seats).to eq 80
+  end
+  it 'serves tequila' do
+    expect(@bar.drinks).to include 'Tequila'
+  end
+  context 'Tequila' do
+    before { @tequila = Drink.new }
+    it 'is a drink' do
+      expect(@tequila).to be_a Drink
+    end
+    it 'costs 4 dollars' do
+      expect(@tequila.price).to eq 4
+    end
   end
 end
